bebb886c937a114a9cd8a0fdf6933b53adc52e9c
stosik <tosikszymon@gmail.com>
2017-11-28 12:44:09 +0100

    Added navbar for admin portal

--- admin-portal/src/app/app.module.ts
+++ admin-portal/src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import 'hammerjs';
+import {
+  MatButtonModule,
+  MatToolbarModule,  
+} from "@angular/material";
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
+const MAT_MODULES  = [
+  MatButtonModule,
+  MatToolbarModule
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    MAT_MODULES
   ],
   providers: [],
   bootstrap: [AppComponent]
